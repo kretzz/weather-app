91edcc6219f4bdebe881570516494be34dc12db0
kretz <joaobernardoc@hotmail.com>
2020-07-07 23:38:50 +0100

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,93 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+//Define paths for Express config
+const publicDirPath = path.join(__dirname, '../public')
+const port = 3001
+const viewPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialsPath)
+
+
+//Setup static directory to serve
+app.use(express.static(publicDirPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'João Castelo',
+
+    })
+})
+
+app.get('/about', (req,res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'João Castelo',
+    })    
+})
+
+//HELP PAGES
+app.get('/help', (req,res) => {
+    res.render('about', {
+        title: 'Help Page',
+        name: 'João Castelo',
+    })    
+})
+
+
+app.get('/help/*',(req, res) => {
+    res.render('404', {
+        error: 'Help article not found',
+        title: 'Help',
+        name: 'João Castelo',
+    })
+})
+
+app.get('/weather', (req, res) => {
+    
+    if(!req.query.address) {
+            return res.send({
+                error: 'You must provide a address term'
+            })
+    }
+
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+        if(error) {
+            return res.send({error})
+        }
+        forecast(latitude,longitude, (error, forecastData) => {
+            if(error) {
+                return res.send({error})
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+//404 handler
+app.get('*', (req, res) => {
+    res.render('404', {
+        error: 'Page not found!',
+        title: '404',
+        name: 'João Castelo',
+    })
+})
+
+app.listen(port, () => {
+    console.log('Server running on port ' + port +'!')
+})
